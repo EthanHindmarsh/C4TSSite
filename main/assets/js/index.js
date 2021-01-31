68b02e460a389b0927407abf9a305079f6a51c51
EthanHindmarsh <ethanhindmarsh04@gmail.com>
2021-01-31 02:51:26 -0600

    Added a neat button - Update to 404

---
+++ main/assets/js/index.js
@@ -0,0 +1,10 @@
+function newCat() {
+    var image = document.getElementById('cat');
+    var images = [
+        "/old/ed.jpg",
+        "/old/cat.jpg",
+        "/old/dog.jpg"
+    ];
+    //https://css-tricks.com/snippets/javascript/select-random-item-array/
+    image.src = images[Math.floor(Math.random()*images.length)];
+}
